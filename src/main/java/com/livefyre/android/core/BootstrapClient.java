5b9e4f97382a9f695c2c5f0e726a813de0e30663
Michael Liao <mliao@livefyre.com>
2014-10-14 16:52:39 -0700

    WIP for getting bootstrap pages

--- src/main/java/com/livefyre/android/core/BootstrapClient.java
+++ src/main/java/com/livefyre/android/core/BootstrapClient.java
@@ -29,8 +29,29 @@ public class BootstrapClient {
                                AsyncHttpResponseHandler handler)
             throws UnsupportedEncodingException
     {
-        final String initEndpoint = generateInitEndpoint(networkId, siteId, articleId);
-        HttpClient.client.get(initEndpoint, handler);
+        getBootstrapPage(networkId, siteId, articleId, handler);
+    }
+
+    /**
+     * Performs a network request on a different thread and delivers a message to the callback.
+     * A JSON object with the results will be bound to the message.
+     *
+     * @param networkId The collection's network as identified by domain, i.e. livefyre.com.
+     * @param siteId    The Id of the article's site.
+     * @param articleId The Id of the collection's article.
+     * @param handler   Response handler
+     * @throws UnsupportedEncodingException
+     * @throws MalformedURLException
+     */
+    public static void getBootstrapPage(String networkId,
+                               String siteId,
+                               String articleId,
+                               AsyncHttpResponseHandler handler,
+                               int... pageNumber)
+            throws UnsupportedEncodingException
+    {
+        final String bootstrapEndpoint = generateBootstrapEndpoint(networkId, siteId, articleId, pageNumber);
+        HttpClient.client.get(bootstrapEndpoint, handler);
     }
 
     /**
@@ -48,19 +69,46 @@ public class BootstrapClient {
                                               String articleId)
             throws UnsupportedEncodingException
     {
+        return generateBootstrapEndpoint(networkId, siteId, articleId);
+    }
+
+    /**
+     * Generates a general bootstrap endpoint with the specified parameters.
+     *
+     * @param networkId The collection's network as identified by domain, i.e. livefyre.com.
+     * @param siteId    The Id of the article's site.
+     * @param articleId The Id of the collection's article.
+     * @return The init endpoint with the specified parameters.
+     * @throws UnsupportedEncodingException
+     * @throws MalformedURLException
+     */
+    public static String generateBootstrapEndpoint(String networkId,
+                                              String siteId,
+                                              String articleId,
+                                              int... pageNumber)
+            throws UnsupportedEncodingException
+    {
         // Casting
         final String article64 = Helpers.generateBase64String(articleId);
 
         // Build the URL
-        final Builder uriBuilder = new Uri.Builder()
+        Builder uriBuilder = new Uri.Builder()
                 .scheme(Config.scheme)
                 .authority(Config.bootstrapDomain + "." + Config.getHostname(networkId))
                 .appendPath("bs3")
                 .appendPath(networkId)
                 .appendPath(siteId)
-                .appendPath(article64)
-                .appendPath("init");
+                .appendPath(article64);
 
+        if (pageNumber.length <= 0) {
+            uriBuilder.appendPath("init");
+        }
+        else {
+            String page = pageNumber[0] + ".json";
+            uriBuilder.appendPath(page);
+        }
+
+        System.out.println(uriBuilder.toString());
         return uriBuilder.toString();
     }
 }
