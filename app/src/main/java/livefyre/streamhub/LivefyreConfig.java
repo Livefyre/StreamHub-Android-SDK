6c424784df6928365450aedbd63baf6d1ab7e331
Kvana <kvanadev1@gmail.com>
2015-08-04 17:35:14 +0530

    SDK with networkID as parameter

---
+++ app/src/main/java/livefyre/streamhub/LivefyreConfig.java
@@ -0,0 +1,25 @@
+package livefyre.streamhub;
+
+public class LivefyreConfig {
+    // comments
+
+    public static String scheme = "http";
+    public static String environment = "livefyre.com";
+    public static String bootstrapDomain = "bootstrap";
+    public static String quillDomain = "quill";
+    public static String adminDomain = "admin";
+    public static String streamDomain = "stream1";
+    //	public static String networkId ="labs.fyre.co";
+    private static String networkID = null;
+
+    public static void setLivefyreNetworkID(String networkID) {
+        LivefyreConfig.networkID = networkID;
+    }
+
+    public static String getConfiguredNetworkID() {
+        if (networkID == null) {
+            throw new AssertionError("You should set Livefyre Network key");
+        }
+        return networkID;
+    }
+}
